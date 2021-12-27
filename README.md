6fa87168aa3a0d10f2831dd1ee7729517151b6b5
FGPRJS <01minted.jowly@icloud.com>
2021-12-27 15:24:00 +0900

    Initial Project
    Project Initialization

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TrashBagPrice
+
 공공데이터를 활용하는 React Project
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path:path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+}
