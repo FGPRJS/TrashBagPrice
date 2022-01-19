a00678353af9434ead2b4fbef81001856226b462
FGPRJS <01minted.jowly@icloud.com>
2022-01-19 11:39:30 +0900

    apply image
    image for result

---
+++ src/entity/PriceProperty.js
@@ -0,0 +1,37 @@
+import TrashBag_1L from "../../resource/TrashBag/TrashBag_1L.svg";
+import TrashBag_1HalfL from "../../resource/TrashBag/TrashBag_1.5L.svg"
+import TrashBag_2L from "../../resource/TrashBag/TrashBag_2L.svg";
+import TrashBag_2HalfL from "../../resource/TrashBag/TrashBag_2.5L.svg"
+import TrashBag_3L from "../../resource/TrashBag/TrashBag_3L.svg";
+import TrashBag_5L from "../../resource/TrashBag/TrashBag_5L.svg";
+import TrashBag_10L from "../../resource/TrashBag/TrashBag_10L.svg";
+import TrashBag_20L from "../../resource/TrashBag/TrashBag_20L.svg";
+import TrashBag_30L from "../../resource/TrashBag/TrashBag_30L.svg";
+import TrashBag_50L from "../../resource/TrashBag/TrashBag_50L.svg";
+import TrashBag_60L from "../../resource/TrashBag/TrashBag_60L.svg";
+import TrashBag_75L from "../../resource/TrashBag/TrashBag_75L.svg";
+import TrashBag_100L from "../../resource/TrashBag/TrashBag_100L.svg";
+import TrashBag_120L from "../../resource/TrashBag/TrashBag_120L.svg";
+import TrashBag_125L from "../../resource/TrashBag/TrashBag_125L.svg";
+
+
+
+const PriceProperty = {
+    "price1" : TrashBag_1L,
+    "price1Half" : TrashBag_1HalfL,
+    "price2" : TrashBag_2L,
+    "price2Half" : TrashBag_2HalfL,
+    "price3" : TrashBag_3L,
+    "price5" : TrashBag_5L,
+    "price10" : TrashBag_10L,
+    "price20" : TrashBag_20L,
+    "price30" : TrashBag_30L,
+    "price50" : TrashBag_50L,
+    "price60" : TrashBag_60L,
+    "price75" : TrashBag_75L,
+    "price100" : TrashBag_100L,
+    "price120" : TrashBag_120L,
+    "price125" : TrashBag_125L,
+}
+
+export default PriceProperty;
