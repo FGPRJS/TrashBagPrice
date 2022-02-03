abc199081390476d847cc358b23097d31f232d94
FGPRJS <01minted.jowly@icloud.com>
2022-02-03 15:09:46 +0900

    Refactoring Label
    Label is class component that parent of tag and price

--- src/entity/Label.jsx
+++ src/entity/Label.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import PriceLabelImg from "../../resource/Label/price.svg";
 import TagLabelImg from "../../resource/Label/tag.svg";
 
-class PriceLabel extends React.Component{
+class Label extends React.Component{
     constructor(props){
         super(props);
         this.state = {
@@ -13,30 +13,22 @@ class PriceLabel extends React.Component{
             }
         }
     }
+}
 
+class PriceLabel extends Label{
     render(){
         return <img style = {this.state.style}  key = "PriceLabel" src = {PriceLabelImg}/>;
     }
 }
 
-class TagLabel extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            style : {
-                width : window.innerWidth/40,
-                height : window.innerHeight/20
-            }
-        }
-    }
-
+class TagLabel extends Label{
     render(){
         return <img style = {this.state.style} key = "TagLabel" src = {TagLabelImg}/>
     }
 }
 
-const Label = {
+const labels = {
     "PriceLabel" : <PriceLabel></PriceLabel>,
     "TagLabel" : <TagLabel></TagLabel>,
 }
-export default Label;
+export default labels;
