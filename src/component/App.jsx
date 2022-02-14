daa27abefcb89906eac4295f6374f9d120773914
FGPRJS <01minted.jowly@icloud.com>
2022-02-14 22:49:52 +0900

    Add Ribbon to additional functions
    add ribbon
    edit ui for ribbon

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -7,17 +7,20 @@ import RegionSelector from "./userinput/RegionSelector.jsx";
 import PriceResult from "./information/PriceResult.jsx";
 import MapController from "./map/MapController.jsx";
 import MapControllerButton from "./map/MapControllerButton.jsx";
+import BookmarkButton from "./userinput/BookmarkButton.jsx";
+import TopRibbon from "./userinput/TopRibbon.jsx";
 
 
 export default function(props){
 
     return <div id = "SuperWrapper">
+        <TopRibbon></TopRibbon>
+
         <div id = "MapWrapper">
             <Mainmap/>
             <RegionSelector></RegionSelector>
             <LocationInfo></LocationInfo>
         </div>
-
         <Loading></Loading>
         <PriceResult></PriceResult>
         <MapControllerButton></MapControllerButton>
