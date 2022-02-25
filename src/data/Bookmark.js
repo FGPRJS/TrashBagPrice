09677d8f99f8cfda38b15da92210fbc226a72dca
FGPRJS <01minted.jowly@icloud.com>
2022-02-25 10:47:25 +0900

    Fixed Bookmark Function
    fixed exception occurs no cookie exists

--- src/data/Bookmark.js
+++ src/data/Bookmark.js
@@ -3,19 +3,13 @@ class Bookmark {
     
     add(new_value){
         this.data.add(new_value);
-        console.log("Current Data : ");
-        console.log(this.data);
     }
     remove(target){
         this.data.delete(target);
-        console.log("Current Data : ");
-        console.log(this.data);
     }
     updateCookie(){
         let result = "";
         for(let d of this.data){
-            console.log("update this cookie : ");
-            console.log(d);
             result += d + ",";
         }
         result = "bookmark=" + result;
@@ -24,14 +18,18 @@ class Bookmark {
     }
     readCookie(){
         let cookie = document.cookie;
+        let bookmarkString = "";
 
-        let bookmarkString = cookie.split('; ')
+        let bookmarkStrings = cookie.split('; ')
         .find(row => row.startsWith('bookmark'))
-        .split('=')[1];
 
+        if(bookmarkStrings != undefined){
+            bookmarkStrings = bookmarkStrings.split('=');
+            bookmarkString = bookmarkStrings[1];
+        }
         this.data = new Set(bookmarkString.split(','));
 
-        return this.data
+        return this.data;
     }
 }
 
